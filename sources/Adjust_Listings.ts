a10407b7f842ce153015154db279309407e87888
ge36ril <armin.stanitzok@tum.de>
2022-09-14 04:51:44 +0200

    Added price trend to item overview

--- sources/Adjust_Listings.ts
+++ sources/Adjust_Listings.ts
@@ -138,6 +138,64 @@ module Adjust_Listings {
 
             adjustBuyOrderListings(<InjectionService.TransferData<BuffTypes.BuyOrder.Data>>transferData);
         }
+
+        if (!document.querySelector('span.buffutility-pricerange')) {
+            console.debug('[BuffUtility] Adjust_Listings (header)');
+            adjustHeaderListings();
+        }
+    }
+
+    async function adjustHeaderListings() {
+        let goods_id = +document.querySelector("div.detail-cont div.add-bookmark").getAttribute('data-target-id');
+        //discard dates from prices as not used
+        let history = (await fetchPriceHistory(goods_id, 30)).map((arr) => arr[1]);
+
+        let header = <HTMLElement>document.querySelector("body > div.market-list > div > div.detail-header.black");
+        let priceMin = history[0], priceMax = history[0];
+
+        for (let i=1;i<history.length;i++) {
+            if (history[i] < priceMin) {
+                priceMin = history[i];
+            }
+            if (history[i] > priceMax) {
+                priceMax = history[i];
+            }
+        }
+
+        let priceParent = <HTMLElement>header.querySelector("div.detail-summ");
+
+        let priceSpan = document.createElement('span');
+        let priceLabel = document.createElement('label');
+        let priceStrong = document.createElement('strong');
+
+        priceParent.setAttribute('style', 'line-height: 200%;');
+        priceSpan.setAttribute('class', 'buffutility-pricerange');
+        priceLabel.innerText = 'Buff Price Trend (7D) |';
+
+        priceStrong.innerHTML= `<strong class='f_Strong'>${Util.convertCNY(priceMin)}<small class="hide-usd">(MIN)</small></strong> — <strong class='f_Strong'>${Util.convertCNY(priceMax)}<small class="hide-usd">(MAX)</small></strong>`;
+
+        priceSpan.appendChild(priceLabel);
+        priceSpan.appendChild(priceStrong);
+        //prevent the double adding of the element caused by the async nature of the function
+        if (!document.querySelector('span.buffutility-pricerange')) {
+            priceParent.appendChild(document.createElement('br'));
+            priceParent.appendChild(priceSpan);
+        }
+    }
+
+    /**
+     * Fetch price history for a given item in the last X days
+     * @param goodsId
+     * @param days
+     * @returns Array of [timestamp, price in rmb] pairs
+     */
+    function fetchPriceHistory(goodsId: number, days: 7 | 30): Promise<[[number, number]]> {
+        return new Promise((resolve, _) => {
+            fetch(`https://buff.163.com/api/market/goods/price_history/buff?game=csgo&goods_id=${goodsId}&days=${days}`)
+            .then(r => r.json().then(_response => {
+                resolve(_response.data.price_history);
+            }));
+        });
     }
 
     function adjustSellOrderListings(transferData: InjectionService.TransferData<BuffTypes.SellOrder.Data>): void {
